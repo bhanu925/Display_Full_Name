f9f23b6174c43aa339b55953701fc126f61187ab
bhanu925 <bhanu.chintu925@gmail.com>
2024-05-06 16:00:00 +0530

    form element added

--- displayname/src/Form/DisplayName.jsx
+++ displayname/src/Form/DisplayName.jsx
@@ -6,41 +6,47 @@ export default function DisplayName() {
     const [display , setDisplay] = useState(false);
     const [firstName , setFirstName] = useState('');
     const [lastName , setLastName] = useState('');
-    const handlesubmit=()=>{
-        setDisplay(true);
+    const handleSubmit=(event)=>{
+        event.preventDefault();
+        if(firstName.trim()!=='' && lastName.trim()!==''){
+            setDisplay(true);
+        }
+        
     }
   return (
   <div className="form">
 
     <h1>Full Name Display</h1>
-    <div>
-        <label htmlFor="firstName">
-            First Name : 
-        </label>
-        <input 
-        type="text" 
-        name="name" 
-        id="firstName" 
-        value={firstName}
-        onChange={(e)=>setFirstName(e.target.value)}
-        />
-    </div>
-    <div>
-        <label htmlFor="lastName">
-            Last Name :
-        </label>
-        <input 
-        type="text"
-        name="lastName" 
-        id="lastName" 
-        value={lastName}
-        onChange={(e)=>setLastName(e.target.value)}
-        />
-    </div> 
+    <form onSubmit={handleSubmit}>
+        <div>
+            <label htmlFor="firstName">
+                First Name : 
+            </label>
+            <input required
+            type="text" 
+            name="name" 
+            id="firstName" 
+            value={firstName}
+            onChange={(e)=>setFirstName(e.target.value)}
+            />
+        </div>
+        <div>
+            <label htmlFor="lastName">
+                Last Name :
+            </label>
+            <input required
+            type="text"
+            name="lastName" 
+            id="lastName" 
+            value={lastName}
+            onChange={(e)=>setLastName(e.target.value)}
+            />
+        </div> 
 
-    <button onClick={handlesubmit} type="submit">
-        submit
-    </button>
+        <button type="submit">
+            submit
+        </button>
+    </form>
     {display && <h3>Full Name : {firstName} {lastName} </h3>}   
     
 </div>)
