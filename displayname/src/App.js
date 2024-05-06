86ea152726cdfbe995489d670fd53057c213cc72
bhanu925 <bhanu.chintu925@gmail.com>
2024-05-06 15:37:38 +0530

    display fullName code

---
+++ displayname/src/App.js
@@ -0,0 +1,11 @@
+import DisplayName from "./Form/DisplayName";
+
+function App() {
+  return (
+    <div className="App">
+     <DisplayName/>
+    </div>
+  );
+}
+
+export default App;
---
+++ displayname/src/Form/DisplayName.jsx
@@ -0,0 +1,51 @@
+import './DisplayName.css';
+import React , { useState } from 'react'
+
+export default function DisplayName() {
+
+    const [display , setDisplay] = useState(false);
+    const [firstName , setFirstName] = useState('');
+    const [lastName , setLastName] = useState('');
+    const handlesubmit=()=>{
+        setDisplay(true);
+    }
+  return (
+  <div className="form">
+
+    <h1>Full Name Display</h1>
+    <div>
+        <label htmlFor="firstName">
+            First Name : 
+        </label>
+        <input 
+        type="text" 
+        name="name" 
+        id="firstName" 
+        value={firstName}
+        onChange={(e)=>setFirstName(e.target.value)}
+        />
+    </div>
+    <div>
+        <label htmlFor="lastName">
+            Last Name :
+        </label>
+        <input 
+        type="text"
+        name="lastName" 
+        id="lastName" 
+        value={lastName}
+        onChange={(e)=>setLastName(e.target.value)}
+        />
+    </div> 
+
+    <button onClick={handlesubmit} type="submit">
+        submit
+    </button>
+    {display && <h3>Full Name : {firstName} {lastName} </h3>}   
+    
+</div>)
+}
+
+
+
+
